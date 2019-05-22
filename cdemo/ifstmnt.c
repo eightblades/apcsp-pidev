943cba52fc73b7dfce4871a125de328bae62ae1b
Trevor Cai <trevorcaisd@gmail.com>
2019-05-22 18:44:14 +0000

    finished

--- cdemo/ifstmnt.c
+++ cdemo/ifstmnt.c
@@ -14,7 +14,6 @@ int main()
     printf("a is not 0\n");
   }
     
-}
   if (a != b)
   {
     printf("a is not equal to b\n");
@@ -24,7 +23,6 @@ int main()
     printf("a is equal to b\n");
   }
     
-}
   if (a > b)
   {
     printf("a is greater than b\n");
@@ -34,7 +32,6 @@ int main()
     printf("a is less than or equal to b\n");
   }
     
-}
   if (a >= 0)
   {
     printf("a is greater than or equal to b\n");
@@ -44,14 +41,22 @@ int main()
     printf("a is less than b\n");
   }
     
-}
   if (a == 0 && b == 0)
   {
-    printf("a is 0\n");
+    printf("Both a and b are equal to 0\n");
   }
   else
   {
-    printf("a is not 0\n");
+    printf("Either a or b or both a and b are no equal to 0\n");
+  }
+    
+if (a == 0 || b == 0)
+  {
+    printf("Either a or b or both a and b are equal to 0\n");
+  }
+  else
+  {
+    printf("Neither a nor b are equal to 0\n");
   }
     
 }
