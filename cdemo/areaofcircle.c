710113153c69c8e95b7c805d4c5f10d50a6e7d00
eightblades <32313740+eightblades@users.noreply.github.com>
2019-05-29 18:46:19 -0700

    Update areaofcircle.c

--- cdemo/areaofcircle.c
+++ cdemo/areaofcircle.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-
-int main()
-{
 float areaOfCircle(int r)
 {
   float a = 3.14*r*r;
   printf("%f\n",a);
   return a;
 }
+
+int main()
+{
+
 for(int i = 3.5; i < 13.5; i++)
 {
   areaOfCircle(i);
