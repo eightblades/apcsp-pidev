e00a46ddee86685dc3fc455e170ee5653cce0ee6
eightblades <32313740+eightblades@users.noreply.github.com>
2019-05-29 18:54:56 -0700

    Update pointers.c

--- cdemo/pointers.c
+++ cdemo/pointers.c
@@ -2,31 +2,32 @@
 
 int main()
 {
-  
-  int a;
-  int* ptrtoa;
+	int a;
+	int* ptrtoa;
 
-  ptrtoa = &a;
+	ptrtoa = &a;
 
-  a = 5;
-  printf("The value of a is %d\n", a);
+	a = 5;
+	printf("The value of a is %d\n", a);
 
-  *ptrtoa = 6;
-  printf("The value of a is %d\n", a);
+	*ptrtoa = 6;
+	printf("The value of a is %d\n", a);
 
-  printf("The value of ptrtoa is %d\n", ptrtoa);
-  printf("It stores the value %d\n", *ptrtoa);
-  printf("The address of a is %d\n", &a);
+	printf("The value of ptrtoa is %d\n", ptrtoa);
+	printf("It stores the value %d\n", *ptrtoa);
+	printf("The address of a is %d\n", &a);
 
-  float d = 0.00;
-  float e = 3.02;
+	float d = 0.003; 
   float* ptrtod = &d;
+	float e = 5.293; 
   float* ptrtoe = &e;
-  printf("d: %f has addr: %d\n", d, &d);
-  printf("e: %f has addr: %d\n", e, &e);
-  float temp = *ptrtod;
-  *ptrtod = *ptrtoe;
-  *ptrtoe = temp;
-  printf("new d = %f\n", d);
-  printf("new e = %f\n", e);
+	printf("The value of d is %f\n", d);
+	printf("The address of d is %d\n", &d);
+	printf("The value of e is %f\n", e);
+        printf("The address of e is %d\n", &e);
+	float tmp = e;
+	e = d;
+	d = tmp;
+	printf("New d: %f\n",d);
+	printf("New e: %f\n",e);
 }
