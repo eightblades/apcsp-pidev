a1b633b48086d4220592927879a642b22b3f4b60
eightblades <32313740+eightblades@users.noreply.github.com>
2019-05-29 18:45:55 -0700

    Update arrays.c

--- cdemo/arrays.c
+++ cdemo/arrays.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-int main(){
-  int arr[100];
-  
-  for (int i = 0; i < 100; i++){
-    arr[i] = i * i;
-  }
   int arrayAdd(int* arr, int s, int n){
     for (int i = 0; i < s; i++){
       arr[i] = arr[i] + n;
       printf("%d\n", arr[i]);
     }
+  }
+int main(){
+  int arr[100];
+  
+  for (int i = 0; i < 100; i++){
+    arr[i] = i * i;
   }
   arrayAdd(arr,100,4);
 }
