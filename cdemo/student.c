71c07088efc3b040aa0437f4a9c50e8e83534e1c
eightblades <32313740+eightblades@users.noreply.github.com>
2019-05-29 20:54:15 -0700

    Update student.c

--- cdemo/student.c
+++ cdemo/student.c
@@ -8,54 +8,74 @@ struct Student{
    int id;
 };
 
-void printStudent(struct Student* student)
-{
+void printStudent(struct Student* student){
   printf("Name: %s %s\n", student->fname, student->lname);
   printf("Age: %d\n", student->age);
   printf("Student ID: %d\n", student->id);
 }
 
-int main()
-{
+int main(){
   char input[256];
   struct Student Stdnt[256];
   char ans;
   int c = 0;
   int num = 1;
 
+  char fname[256];
+	char lname[256];
+	int age;
+	int id;
+
   while (c == 0){
-    printf("Enter Student(Y/N)? ");
+    printf("View or Enter? ");
     fgets(input, 256, stdin);
     sscanf(input, "%s", &ans);
-    if (strcmp(&ans, "y") == 0 || strcmp(&ans, "Y") == 0){
-	    char fname[256];
-	    char lname[256];
-	     int age;
-	     int id;
+    if (strcmp(&ans, "enter") == 0 || strcmp(&ans, "Enter") == 0){
+
 	    printf("Enter first name: ");
 	    fgets(input, 256, stdin);
 	    sscanf(input, "%s", fname);
+
 	    printf("Enter last name: ");
 	    fgets(input, 256, stdin);
 	    sscanf(input, "%s", lname);
+
 	    printf("Enter age: ");
-	    fgets(input, 256, stdin);
-	    sscanf(input, "%d", age);
+      while (1){
+	      fgets(input, 256, stdin);
+	      if (sscanf(input, "%d", &age) == 1) break;
+        printf("Invalid Input\n");
+      }
 	    printf("Enter student ID ");
-	    fgets(input, 256, stdin);
- 	    sscanf(input, "%d", id);
+      while (1){
+	      fgets(input, 256, stdin);
+ 	      if (sscanf(input, "%d", &id) == 1) break;
+        printf("Invalid Input\n");
+      }
+
 	    strcpy(Stdnt[num].fname, fname);
 	    strcpy(Stdnt[num].lname, lname);
 	    Stdnt[num].age = age;
 	    Stdnt[num].id = id;
 	    num++;
-    } else{
+    } else if(strcmp(&ans, "view") == 0 || strcmp(&ans, "View") == 0){
 	    c = 1;
+    } else{
+      printf("Invalid Input\n");
     }
   }
   int n;
-	    printf("View - Student Number:  ");
-	    fgets(input, 256, stdin);
-	    sscanf(input, "%d", n);
-	    printStudent(&Stdnt[n]);
+  int b = 0;
+  while (c == 1){
+	    printf("View - Student Number: ");
+	    while (1){
+	      fgets(input, 256, stdin);
+ 	      if (sscanf(input, "%d", &n) == 1) break;
+        printf("Invalid Input\n");
+      }
+      printf("Name: %s %s\n", fname, lname);
+      printf("Age: %d\n", age);
+      printf("Student ID: %d\n", id);
+      c = 0;
+  }
 }
