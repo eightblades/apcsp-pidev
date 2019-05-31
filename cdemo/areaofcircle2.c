e1e134ddad40a7cc277eecd955486c29b07acf07
Trevor Cai <trevorcaisd@gmail.com>
2019-05-31 18:32:07 +0000

    .

--- cdemo/areaofcircle2.c
+++ cdemo/areaofcircle2.c
@@ -6,12 +6,31 @@ float areaOfCircle(float r) {
 }
 
 int main(int argc, char* argv[]){
-  int arg = 0;
+  int arg = 1;
   while (arg < argc){
     printf("Radius %d : %s\n", arg, argv[arg]);
     arg++;
   }
+  float arg1;
+  int found = sscanf(argv[1], "%f", &arg1);
+  if (argc == 3){
   
+  if (found != 1){
+    printf("first arg is not a number, enter two nums\n");
+    return 1;
+  }
+  
+  float arg2;
+  found = sscanf(argv[2], "%f", &arg2);
+  if (found != 1){
+    printf("second arg is not an number, enter two nums\n");
+    return 1;
+  }
+    for (float r = arg1; r <= arg2; r++){
+      float area = areaOfCircle(r);
+      printf("Radius = %f - Area: %f\n", r, area); 
+  }
+  }else{
   float l;
   float u;
   int c = 1;
@@ -31,7 +50,7 @@ int main(int argc, char* argv[]){
     scanf("%f", &u);
     if ( u < 0 ){
       printf("Not a valid bound\n");
-    }else if( u <= l ){
+      }else if( u <= l ){
     printf("not a valid bound\n");
     }else{
       c = 2;
@@ -39,7 +58,9 @@ int main(int argc, char* argv[]){
   }
   
   for (float r = l; r <= u; r++){
-	  float area = areaOfCircle(r);
-	  printf("Radius = %f - Area: %f\n", r, area);
+    float area = areaOfCircle(r);
+    printf("Radius = %f - Area: %f\n", r, area);
   }
 }
+}
+
